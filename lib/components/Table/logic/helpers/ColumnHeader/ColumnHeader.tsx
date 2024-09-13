ca6afd4c3eea6fcea4aba8d7db4cc895bb86fbb1
Tal Kohavy <talkohavy@gmail.com>
2024-09-13 20:54:27 +0300

    columnHeader has display flex now

--- lib/components/Table/logic/helpers/ColumnHeader/ColumnHeader.tsx
+++ lib/components/Table/logic/helpers/ColumnHeader/ColumnHeader.tsx
@@ -11,7 +11,7 @@ export default function ColumnHeader(props: ColumnHeaderProps) {
   const { table, header, showCheckbox = true } = props;
 
   return (
-    <>
+    <div className={styles.columnHeader}>
       {showCheckbox && (
         <IndeterminateCheckbox
           checked={table.getIsAllRowsSelected()}
@@ -21,6 +21,6 @@ export default function ColumnHeader(props: ColumnHeaderProps) {
       )}
 
       <div className={styles.columnHeaderTitle}>{header}</div>
-    </>
+    </div>
   );
 }
