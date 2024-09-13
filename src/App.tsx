2fbaa6339113768a5e844ef94adda64cc9c039e2
Tal Kohavy <talkohavy@gmail.com>
2024-09-13 03:28:40 +0300

    created a dark mode version

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div
       style={{
-        backgroundColor: 'white',
+        backgroundColor: '#383838',
         width: '100%',
         height: '100%',
         padding: 40,
