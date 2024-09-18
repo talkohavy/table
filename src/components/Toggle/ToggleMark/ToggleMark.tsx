4b2dd588f6ae202128cb838b0f4e1d50ca30a32e
Tal Kohavy <talkohavy@gmail.com>
2024-09-18 09:02:43 +0300

    Toggle style made better

--- src/components/Toggle/ToggleMark/ToggleMark.tsx
+++ src/components/Toggle/ToggleMark/ToggleMark.tsx
@@ -3,12 +3,15 @@ import styles from './ToggleMark.module.scss';
 
 type ToggleMarkProps = {
   isChecked: boolean;
+  className?: string;
 };
 
 export default function ToggleMark(props: ToggleMarkProps) {
-  const { isChecked } = props;
+  const { isChecked, className } = props;
 
   return (
-    <div className={clsx(styles.toggleMark, isChecked ? styles.toggleMarkChecked : styles.toggleMarkUnchecked)}></div>
+    <div
+      className={clsx(styles.toggleMark, isChecked ? styles.toggleMarkChecked : styles.toggleMarkUnchecked, className)}
+    ></div>
   );
 }
