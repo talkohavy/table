f18bfc8e0c00d62678f906757c60bf7cc68b13f6
Tal Kohavy <talkohavy@gmail.com>
2024-09-28 12:12:24 +0300

    RELEASE-3

---
+++ static/js/index.BPmFXC9H.js
@@ -0,0 +1,2 @@
+import{j as t}from"../../main/index-CmgFUGfv.js";import{T as e,m as s}from"./mockData.CzcPj3eY.js";function r(){return t.jsx("div",{className:"flex flex-col justify-start items-start gap-4 size-full p-10",children:t.jsx("div",{className:"w-full",children:t.jsx(e,{data:s,isFullSize:!0,showFooter:!0})})})}export{r as default};
+//# sourceMappingURL=index.DC_zFhJl.js.map
---
+++ static/js/types.CIHhg7CB.js
@@ -0,0 +1,2 @@
+var r=(n=>(n.Single="single",n.Multi="multi",n.None="none",n))(r||{});export{r as R};
+//# sourceMappingURL=types.BQ4uE7aj.js.map
