49a61211a94861b23bd8b343d8fe923bb59df2df
Tal Kohavy <talkohavy@gmail.com>
2025-05-12 01:12:10 +0300

    BUGFIX: RowSelectionMode wrong oath in index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,5 @@ export { default as Table } from './components/Table';
 export { default as TableFooter } from './components/Table/parts/TableFooter/TableFooter.js';
 
 export { createColumnHelper } from '@tanstack/react-table';
-export { RowSelectionMode, type DefaultColumn } from './components/Table/types.js';
+export { type DefaultColumn } from './components/Table/types.js';
+export { RowSelectionMode } from './components/Table/logic/constants.js';
