5996014c8734e7a062f573ab16fcd7dc5f973734
Tal Kohavy <talkohavy@gmail.com>
2025-05-12 13:19:11 +0300

    RELEASE-6

--- static/js/index.DqMW_QB0.js
+++ static/js/index.DqMW_QB0.js
@@ -1,4 +1,4 @@
-import{j as a,c as e}from"../../main/index-0VEn62tx.js";import{T as l,m as s}from"./mockData.Bdcux-w-.js";import{C as t}from"./CodeBlock.Bmbirc4p.js";const m="_myTable_ytcpk_1",o={myTable:m};function p(){return a.jsxs("div",{className:"flex flex-col justify-start items-start gap-4 size-full p-10",children:[a.jsx("div",{className:"w-full",children:a.jsx(l,{data:s,className:e("private-table",o.myTable)})}),a.jsx(t,{className:"w-full border",language:"typescript",code:`import clsx from 'clsx';
+import{j as a,c as e}from"../../main/index-Dj9nCv4v.js";import{T as l,m as s}from"./mockData.D_FcDWWK.js";import{C as t}from"./CodeBlock.mILqvtuj.js";const m="_myTable_ytcpk_1",o={myTable:m};function p(){return a.jsxs("div",{className:"flex flex-col justify-start items-start gap-4 size-full p-10",children:[a.jsx("div",{className:"w-full",children:a.jsx(l,{data:s,className:e("private-table",o.myTable)})}),a.jsx(t,{className:"w-full border",language:"typescript",code:`import clsx from 'clsx';
 import { Table } from '../../../lib';
 import CodeBlock from '../../components/CodeBlock';
 import { mockData } from '../../mockData';
