1871f1178bec7fb0e5a55a53f30020d05a0abe6c
Tal Kohavy <talkohavy@gmail.com>
2025-05-12 07:19:49 +0300

    using CodeBlock

--- src/pages/SimpleTablePage/SimpleTablePage.tsx
+++ src/pages/SimpleTablePage/SimpleTablePage.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import { Table } from '../../../lib';
+import CodeBlock from '../../components/CodeBlock';
 import { mockData } from '../../mockData';
 import styles from './SimpleTablePage.module.scss';
 
@@ -9,6 +10,27 @@ export default function SimpleTablePage() {
       <div className='w-full'>
         <Table data={mockData} className={clsx('private-table', styles.myTable)} />
       </div>
+
+      <CodeBlock
+        className='w-full border'
+        language='typescript'
+        code={`import clsx from 'clsx';
+import { Table } from '../../../lib';
+import CodeBlock from '../../components/CodeBlock';
+import { mockData } from '../../mockData';
+import styles from './SimpleTablePage.module.scss';
+
+export default function SimpleTablePage() {
+  return (
+    <div className='flex flex-col justify-start items-start gap-4 size-full p-10'>
+      <div className='w-full'>
+        <Table data={mockData} className={clsx('private-table', styles.myTable)} />
+      </div>
+    </div>
+  );
+}
+`}
+      />
     </div>
   );
 }
