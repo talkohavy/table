65eb83c5426fd32707e535b5cbb366fd2cad9010
Tal Kohavy <talkohavy@gmail.com>
2025-05-12 23:49:37 +0300

    Youe mustnt import css style files from a bootstrap, or else they will be shaken

--- src/common/bootstrap.ts
+++ src/common/bootstrap.ts
@@ -1,2 +1,1 @@
-import './styles/dark-mode-table.css';
 import '../lib/syntaxHighlighter';
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import DarkThemeProvider from './providers/DarkThemeProvider';
+// Static styles must be imported in the main entry, and NOT from some bootstrap file, to ensure they are included in the build:
 import './common/bootstrap';
+import './common/styles/dark-mode-table.css'; // Import dark-mode CSS directly here
+import './lib/syntaxHighlighter/styles/1c-light.css';
+import './lib/syntaxHighlighter/styles/vscode-dark.css';
 import './index.css';
 
 function Client() {
