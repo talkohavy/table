02ce652249f43a8b3f5e65f97d7e17f4c7a686db
Tal Kohavy <talkohavy@gmail.com>
2025-05-12 23:50:41 +0300

    RELEASE-7

---
+++ static/js/index.CjOg_7R_.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-Cfjnx_bC.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=../../sourcemaps/index.DuqGtDIm.js.map
