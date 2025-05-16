4f456323a6fb628b3ca88eea130663b58e418cea
Tal Kohavy <talkohavy@gmail.com>
2025-05-16 14:30:20 +0300

    RELEASE-18

---
+++ static/js/index.yx1kS29r.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-CFuHQmjs.js";function s(){const e=t();return a.useEffect(()=>{e(`${o}/simple-table`)},[e]),null}export{s as default};
+//# sourceMappingURL=../../sourcemaps/index.BTWUM2kI.js.map
