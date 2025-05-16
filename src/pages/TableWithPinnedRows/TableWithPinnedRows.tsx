a1c24f735dba597d9319e4902b9cbe9166f55723
Tal Kohavy <talkohavy@gmail.com>
2025-05-16 14:48:49 +0300

    CodeBlock showed wrong code for TableWithPinnedRows page

--- src/pages/TableWithPinnedRows/TableWithPinnedRows.tsx
+++ src/pages/TableWithPinnedRows/TableWithPinnedRows.tsx
@@ -1,5 +1,5 @@
 import { createColumnHelper } from '@tanstack/react-table';
-import { Table } from '../../../lib/index.ts';
+import { Table } from '../../../lib';
 import CodeBlock from '../../components/CodeBlock/CodeBlock.tsx';
 import { mockData } from '../../mockData.ts';
 import styles from './TableWithPinnedRows.module.scss';
@@ -40,15 +40,31 @@ export default function TableWithPinnedRows() {
 
       <CodeBlock
         language='typescript'
-        className='w-full border'
+        className='w-full'
         code={`import { createColumnHelper } from '@tanstack/react-table';
 import { Table } from '../../../lib';
 import { mockData } from '../../mockData.ts';
-import styles from './TableWithCustomTitles.module.scss';
+import styles from './TableWithPinnedRows.module.scss';
+
+const includeLeafRows = false;
+const includeParentRows = false;
 
 const columnHelper = createColumnHelper<any>();
 
 const columnDefsRaw = [
+  columnHelper.accessor('pin', {
+    id: 'pin',
+    header: 'Pin',
+    cell: ({ row }) =>
+      row.getIsPinned() ? (
+        <button onClick={() => row.pin(false, includeLeafRows, includeParentRows)}>❌</button>
+      ) : (
+        <div style={{ display: 'flex', gap: '4px' }}>
+          <button onClick={() => row.pin('top', includeLeafRows, includeParentRows)}>⬆️</button>
+          <button onClick={() => row.pin('bottom', includeLeafRows, includeParentRows)}>⬇️</button>
+        </div>
+      ),
+  }),
   columnHelper.accessor('id', { header: 'ID' }),
   columnHelper.accessor('first_name', { header: 'First Name' }),
   columnHelper.accessor('last_name', { header: 'Last Name' }),
@@ -57,7 +73,7 @@ const columnDefsRaw = [
   columnHelper.accessor('ip_address', { header: 'IP Address' }),
 ];
 
-export default function TableWithCustomTitles() {
+export default function TableWithPinnedRows() {
   return (
     <div className='flex flex-col justify-start items-start gap-4 size-full p-10'>
       <div className='w-full'>
